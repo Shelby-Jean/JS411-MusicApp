b92b35dbf87e67e87fa71c93bf0453b12fe469b9
Shelby-Jean <shelby.gottschall@gmail.com>
2020-11-23 18:38:47 -0500

    login screen complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
+import { TextField, Button } from '@material-ui/core';
+import NavBar from './components/NavBar';
+import Dashboard from './components/Dashboard';
+import 'fontsource-roboto';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    loggedIn: false,
+  }
+
+  login = () => {
+    this.setState({
+      loggedIn: true,
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <NavBar />
+        
+        {this.state.loggedIn ? <Dashboard /> : (
+         <div className="form-container">
+          <form className="login-form">
+            <TextField id="standard-basic" label="Username" required />
+            <TextField id="standard-basic" label="Password" type="password" required />
+            <br/>
+            <Button variant="contained" color="primary" onClick={this.login}>LOGIN</Button>
+          </form>
+        </div>
+        )}
+        
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Cards, CardContent, CardActions } from '@material-ui/core';
+
+const Dashboard = () => {
+  return (
+    <div>
+      hello
+    </div>
+  )
+}
+
+export default Dashboard;
